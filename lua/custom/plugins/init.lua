02a461cd39bd49ab014c7d522a5d8356fdcbcb35
mitchelldyer01@gmail.com <mitchelldyer01@gmail.com>
2024-03-10 12:15:02 -0700

    include organize_imports

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,16 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
 return {
   'tpope/vim-fugitive',
+
+  organize_imports = function()
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = '*.go',
+      callback = function()
+        vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
+      end,
+    })
+  end,
 }
